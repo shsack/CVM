2515822479a68c0f4f3c062a45312bc378b56103
ssack <ssack@student.ethz.ch>
2019-02-13 15:43:57 +0100

    Changed to Conjugate Gradient Squared Method

--- cvm.cc
+++ cvm.cc
@@ -7,8 +7,8 @@
 using namespace itensor;
 using namespace std;
 
-const int N = 10;
-const int maxm = 100;
+const int N = 5;
+const int maxm = 50;
 const double cut = 1E-6;
 auto sites = Hubbard(N, {"ConserveNf", false}); // Define Hubbard model
 
@@ -47,34 +47,39 @@ double ground_state(MPS & psi, MPO H)
 }
 
 
-MPS conj_gradient(MPO H, complex<double> z, MPS b)
+MPS conjugate_gradient_squared(MPO H, complex<double> z, MPS b)
 {
+
     auto x = b; // initialize |x> with |b> to iteratively solve A|x> = |b>
     MPS r_old = sum(b, -1 * sum(applyMPO(H, x, args), -z * x, args));
+    MPS r_ = r_old;
     MPS p = r_old;
+    MPS u = r_old;
+    MPS q;
     MPS r_new;
-    MPS Ap = sum(applyMPO(H, p, args), -z * p, args);
+    MPS Ap;
     complex<double> alpha;
     complex<double> beta;
-    MPS tmp1;
-    MPS tmp2;
+    MPS u_q;
+
+    Print(overlapC(r_old, r_));
 
     for(int i = 0; i < 10; ++i){
 
-        tmp1 = sum(applyMPO(H, r_old, args),  -z * r_old, args);
-        alpha = overlapC(r_old, tmp1) / overlapC(Ap, Ap);
-        x = sum(x, alpha * p, args);
-        r_new = sum(r_old, -alpha * Ap, args);
-        tmp2 = sum(applyMPO(H, r_new, args), -z * r_new, args);
-        beta = overlapC(r_new, tmp2) / overlapC(r_old, tmp1);
-        p = sum(r_new, beta * p, args);
-        Ap = sum(tmp2, beta * Ap, args);
+        Ap = sum(applyMPO(H, p, args), -z * p, args);
+        alpha = overlapC(r_old, r_) / overlapC(Ap, r_);
+        q = sum(u, -alpha * Ap, args);
+        u_q = sum(u, q, args);
+        x = sum(x, alpha * u_q, args);
+        r_new = sum(r_old, -alpha * sum(applyMPO(H, u_q, args), -z * u_q, args), args);
+        beta = overlapC(r_new, r_) / overlapC(r_old, r_);
+        u = sum(r_new, beta * q, args);
+        p = sum(u, beta * sum(q, beta * p, args), args);
         r_old = r_new;
 
-        Print(norm(r_old));
+        Print(sqrt(overlapC(r_old, r_old)));
 
     }//while(abs(norm(r_old) - 1) > 1E-3);
-
 
     return x;
 }
@@ -86,20 +91,25 @@ int main()
     auto H = Hamiltonian();
     auto energy = ground_state(psi, H);
 
-    const double omega = 0.0;
-    const double eta = 0.01;
+    Print(energy);
+
+    const double omega = 0.5;
+    const double eta = 0.1;
     const complex<double> z(energy + omega, eta);
 
 //  Solve (H - z)|x> = c|Gs> by iteratively solving A|x> = b
 
-    int i = 5;
-    int j = 5;
+    int i = 1;
+    int j = 1;
 
-
+    // c_dagger |Gs>
     auto b = psi;
     b.position(i);
     b.setA(i, (b.A(i) * sites.op("Cdagup", i)).noprime());
-    auto x = conj_gradient(H, z, b);
+
+    auto x = conjugate_gradient_squared(H, z, b);
+
+    // c |x>
     auto c_x = x;
     c_x.position(j);
     c_x.setA(j, (c_x.A(j) * sites.op("Cdn", j)).noprime());
