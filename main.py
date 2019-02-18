e60d7f24a245ff7dfae148234eb6b0eeb0b1ff7b
ssack <ssack@student.ethz.ch>
2019-02-18 17:47:47 +0100

    Implemented a for loop to call the executable for all energies and store the result in one file

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import numpy as np
 import os
+import csv
 
-omega = np.linspace(start=0, stop=3, num=10)
+omega = np.linspace(start=0, stop=3, num=2, dtype=float)
 
 for om in omega:
     os.system("./cvm {} 0.1 10".format(om))
+
+correlator = []
+
+for om in omega:
+
+    name = "data/cvm_{}.txt".format(om)
+    f = open(name)
+    correlator.append(float(f.readline()))
+    f.close()
+    os.remove(name)
+
+print(correlator)
+
+f = open('data/cvm_data.csv', 'w')
+out = csv.writer(f, delimiter=' ')
+out.writerows(zip(omega, correlator))
+f.close()
