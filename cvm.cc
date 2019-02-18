2306beee4084569d98a215bd562abd3cc112550b
ssack <ssack@student.ethz.ch>
2019-02-18 12:18:50 +0100

    Added identy operator and changed to paramters read from terminal

--- cvm.cc
+++ cvm.cc
@@ -6,9 +6,8 @@
 #include <math.h>
 
 using namespace itensor;
-using namespace std;
 
-const int N = 7;
+const int N = 6;
 const int maxm = 100;
 const double cut = 1E-6;
 // auto sites = Hubbard(N, {"ConserveNf", false}); // Define Hubbard model
@@ -19,8 +18,7 @@ auto sites = SpinHalf(N);
 auto args = Args({"Maxm", maxm, "Cutoff", cut});
 
 
-MPO Hamiltonian()
-{
+MPO Hamiltonian() {
 /*    double U = 1.0;
     double t = 1.0;
 
@@ -40,8 +38,8 @@ MPO Hamiltonian()
     return MPO(ampo);*/
 
     auto ampo = AutoMPO(sites);
-    for(int j = 1; j < N; ++j)
-    {
+    for(int j = 1; j < N; ++j) {
+
         ampo += "Sz",j,"Sz",j+1;
         ampo += 0.5,"S+",j,"S-",j+1;
         ampo += 0.5,"S-",j,"S+",j+1;
@@ -49,17 +47,21 @@ MPO Hamiltonian()
     return MPO(ampo);
 }
 
-MPO Sz(int i)
-{
+MPO Sz(int i) {
     auto ampo = AutoMPO(sites);
     ampo += "Sz", i;
     return MPO(ampo);
 }
 
 
+MPO I() {
+    auto ampo = AutoMPO(sites);
+    ampo += "Id", 1;
+    return MPO(ampo);
+}
 
-double ground_state(MPS & psi, MPO H)
-{
+
+double ground_state(MPS & psi, MPO H) {
     auto sweeps = Sweeps(10);
     sweeps.maxm() = 10, 20, 50, 70; //gradually increase states kept
     sweeps.cutoff() = cut; //desired truncation error
@@ -68,31 +70,31 @@ double ground_state(MPS & psi, MPO H)
 }
 
 
-MPS conjugate_gradient_squared(MPO H, complex<double> z, MPS b)
-{
+MPS conjugate_gradient_squared(MPO A, MPS b, int iter) {
+    // Can I make an A MPO?
 
     auto x = b; // initialize |x> with |b> to iteratively solve A|x> = |b>
-    MPS r_old = sum(b, -1 * sum(applyMPO(H, x, args), -z * x, args));
+    MPS r_old = sum(b, -1 * applyMPO(A, x, args));
     MPS r_ = r_old;
     MPS p = r_old;
     MPS u = r_old;
     MPS q;
     MPS r_new;
     MPS Ap;
-    complex<double> alpha;
-    complex<double> beta;
+    std::complex<double> alpha;
+    std::complex<double> beta;
     MPS u_q;
 
     // Print(overlapC(r_old, r_));
 
-    for(int i = 0; i < 50; ++i){
+    for(int i = 0; i < iter; ++i){
 
-        Ap = sum(applyMPO(H, p, args), -z * p, args);
+        Ap = applyMPO(A, p, args);
         alpha = overlapC(r_old, r_) / overlapC(Ap, r_);
         q = sum(u, -alpha * Ap, args);
         u_q = sum(u, q, args);
         x = sum(x, alpha * u_q, args);
-        r_new = sum(r_old, -alpha * sum(applyMPO(H, u_q, args), -z * u_q, args), args);
+        r_new = sum(r_old, -alpha * applyMPO(A, u_q, args), args);
         beta = overlapC(r_new, r_) / overlapC(r_old, r_);
         u = sum(r_new, beta * q, args);
         p = sum(u, beta * sum(q, beta * p, args), args);
@@ -100,37 +102,42 @@ MPS conjugate_gradient_squared(MPO H, complex<double> z, MPS b)
 
     }
 
-    cout << "\nResidue = " << norm(r_old) << endl;
+    std::cout << "\nResidue = " << norm(r_old) << std::endl;
 
     return x;
 }
 
-double spectral_function(MPS psi, MPO H, MPO (*Sz)(int), double omega, double eta, complex<double> z, int i, int j)
-{
-    auto x = conjugate_gradient_squared(H, z, applyMPO(Sz(j), psi, args));
-    complex<double> G = overlapC(psi, applyMPO(Sz(i), x, args));
+double spectral_function(MPS psi, MPO H, MPO (*Sz)(int), double omega, double eta, double energy, int iter, int i, int j) {
 
-    return - G.imag() / M_PI;
+    const std::complex<double> z(omega + energy, eta);
+    auto A = sum(H, -z * I(), args);
+    auto b =  applyMPO(Sz(j), psi, args);
+    auto x = conjugate_gradient_squared(A, b, iter);
+
+    std::complex<double> G = overlapC(psi, applyMPO(Sz(i), x, args));
+
+    return G.imag() / M_PI;
 };
 
 
+int main(int argc, char* argv[]) {
 
-
-int main()
-{
     auto psi = MPS(sites);
     auto H = Hamiltonian();
     auto energy = ground_state(psi, H);
 
-    const double omega = 0.;
-    const double eta = 0.1;
-    const complex<double> z(omega + energy, -eta);
+    const double omega = std::atof(argv[1]);
+    const double eta = std::atof(argv[2]);
+    const int iter = std::atoi(argv[3]);
 
     int i = 2;
     int j = 3;
 
-    double A = spectral_function(psi, H, Sz, omega, eta, z, i, j);
-    cout << "\nA = " << A << endl;
+    double A = spectral_function(psi, H, Sz, omega, eta, energy, iter, i, j);
+
+    std::cout << "\nomega = " << omega << std::endl;
+    std::cout << "\neta = " << eta << std::endl;
+    std::cout << "\nA = " << A << std::endl;
 
     return 0;
 }
