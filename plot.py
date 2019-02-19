3fdeec0bfcd2edcfafcbc9556cb34e27253a5a05
ssack <ssack@student.ethz.ch>
2019-02-19 09:51:20 +0100

    Implemented script for plotting

---
+++ plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = 'data/cvm_data.csv'
+data = pd.read_csv(file, delimiter=' ', header=None, dtype=float)
+plt.plot(data[0], abs(data[1]))
+plt.show()
