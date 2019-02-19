c0f2241dc5a972536c154c5a29332c202b348c5c
ssack <ssack@student.ethz.ch>
2019-02-19 14:58:45 +0100

    Added an initial state for the DMRG

--- cvm.cc
+++ cvm.cc
@@ -11,8 +11,8 @@
 using namespace itensor;
 
 const int N = 3;
-const int maxm = 100;
-const double cut = 1E-6;
+const int maxm = 1000;
+const double cut = 1E-13;
 // auto sites = Hubbard(N, {"ConserveNf", false}); // Define Hubbard model
 
 auto sites = SpinHalf(N);
@@ -58,23 +58,43 @@ MPO Sx(int i) {
 
 
 MPO I() {
+
     auto ampo = AutoMPO(sites);
-    ampo += "Id", 1;
-    return MPO(ampo);
+    for(int i = 1; i <= N; ++i){
+
+        ampo += "Id", i;
+
+    }
+
+    return (1./N) * MPO(ampo);
+
 }
 
 
 double ground_state(MPS & psi, MPO H) {
-    auto sweeps = Sweeps(10);
+
+    auto sweeps = Sweeps(5);
     sweeps.maxm() = 10, 20, 50, 70; //gradually increase states kept
     sweeps.cutoff() = cut; //desired truncation error
 
     return dmrg(psi, H, sweeps, "Quiet");
 }
 
+MPS conjugate_(MPS psi){
+
+    auto tmp = psi;
+
+    for(int i = 1; i <= N; ++i){
+
+        tmp.Aref(i).conj();
+
+    }
+
+    return tmp;
+}
+
 
 MPS conjugate_gradient_squared(MPO A, MPS b, int iter) {
-    // Can I make an A MPO?
 
     auto x = b; // initialize |x> with |b> to iteratively solve A|x> = |b>
     MPS r_old = sum(b, -1 * applyMPO(A, x, args));
@@ -86,6 +106,11 @@ MPS conjugate_gradient_squared(MPO A, MPS b, int iter) {
     MPS Ap;
     std::complex<double> alpha;
     std::complex<double> beta;
+
+    //
+    //alpha = 0.0;
+    //auto alpha = std::complex<long double>(std::real(0), std::imag(0));
+
     MPS u_q;
 
     // Print(overlapC(r_old, r_));
@@ -93,12 +118,28 @@ MPS conjugate_gradient_squared(MPO A, MPS b, int iter) {
     for(int i = 0; i < iter; ++i){
 
         Ap = applyMPO(A, p, args);
-        alpha = overlapC(r_old, r_) / overlapC(Ap, r_);
+        alpha = overlapC(conjugate_(r_old), r_) / overlapC(conjugate_(Ap), r_); // print
+
+        // Print(alpha);
+
+        //Print(overlapC(conjugate_(Ap), r_));
+
+        std::cout << alpha << std::endl;
+
         q = sum(u, -alpha * Ap, args);
         u_q = sum(u, q, args);
         x = sum(x, alpha * u_q, args);
         r_new = sum(r_old, -alpha * applyMPO(A, u_q, args), args);
-        beta = overlapC(r_new, r_) / overlapC(r_old, r_);
+        beta = overlapC(conjugate_(r_new), r_) / overlapC(conjugate_(r_old), r_); // prin
+
+
+        // Print(beta);
+
+        // Print(overlapC(conjugate_(r_old), r_));
+
+        std::cout << beta << std::endl;
+
+
         u = sum(r_new, beta * q, args);
         p = sum(u, beta * sum(q, beta * p, args), args);
         r_old = r_new;
@@ -113,19 +154,30 @@ MPS conjugate_gradient_squared(MPO A, MPS b, int iter) {
 double spectral_function(MPS psi, MPO H, MPO (*Sx)(int), double omega, double eta, double energy, int iter, int i, int j) {
 
     const std::complex<double> z(omega + energy, eta);
-    auto A = sum(z * I(), -1 * H, args);
+    auto A = sum(z * I(), -1. * H, args);
     auto b =  applyMPO(Sx(j), psi, args);
     auto x = conjugate_gradient_squared(A, b, iter);
 
     std::complex<double> G = overlapC(psi, Sx(i), x);
 
-    return G.imag() / M_PI;
+    return -G.imag() / M_PI;
 };
 
 
 int main(int argc, char* argv[]) {
 
-    auto psi = MPS(sites);
+    // auto psi = MPS(sites);
+
+    InitState state(sites,"Up");
+//Now set every other spin to be Dn
+    for(int j = 2; j <= N; j += 2)
+    {
+        state.set(j,"Dn");
+    }
+
+    MPS psi(state);
+
+
     auto H = Hamiltonian();
     auto energy = ground_state(psi, H);
 
@@ -143,7 +195,8 @@ int main(int argc, char* argv[]) {
 
 */
 
-/*    std::string str = std::to_string (omega);
+/*
+    std::string str = std::to_string (omega);
     str.erase ( str.find('.') + 2, std::string::npos );
 */
 
