Fixed sorting filenames numerically

main.py
@@ -5,6 +5,8 @@ from mpi4py import MPI
 import multiprocessing as mp
 from functools import partial
 import glob
+from decimal import Decimal
+import re
 
 
 def split_data(size, data):
@@ -27,12 +29,8 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()  # Identification number of node
 size = comm.Get_size()  # Number of nodes
 
-# Define simulation parameters
-omega = np.linspace(start=-1., stop=4., num=4*4*4, dtype=float)
-eta = 0.05
-num_iter = 5
-i = 1
-j = 1
+# Define data
+omega = np.linspace(start=-2., stop=4., num=int(40/0.05), dtype=float)
 
 # Split the data in the zeroth node
 if rank == 0:
@@ -45,7 +43,7 @@ data_in_node = comm.scatter(data_split, root=0)
 
 # Split running of exe in each rank on CPUs
 p = mp.Pool(mp.cpu_count())
-run_exe_ = partial(run_exe, eta=eta, num_iter=num_iter, i=i, j=j)
+run_exe_ = partial(run_exe, eta=0.05, num_iter=3, i=1, j=1)
 p.map(run_exe_, data_in_node)
 
 
@@ -54,14 +52,18 @@ correlator = []
 # Make sure that all ranks have finished
 comm.gather(0, root=0)
 
+# Sort the filenames numerically
+filenames = sorted([(element[9:-4]) for element in glob.glob('data/*.txt')], key=float)
+
 if rank == 0:
 
-    for filename in sorted(glob.glob('data/*.txt')):
+    for filename in filenames:
 
-        f = open(filename)
+        name = 'data/cvm_{}.txt'.format(filename)
+        f = open('data/cvm_{}.txt'.format(filename))
         correlator.append(float(f.readline()))
         f.close()
-        os.remove(filename)
+        os.remove(name)
 
         # Save data in file
     f = open('data/cvm_data.csv', 'w')