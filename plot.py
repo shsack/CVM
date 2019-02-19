8cc8df0396c9ce024eb17072cf9fa128f87ac8c9
ssack <ssack@student.ethz.ch>
2019-02-19 15:31:49 +0100

    No abs

--- plot.py
+++ plot.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 file = 'data/cvm_data.csv'
 data = pd.read_csv(file, delimiter=' ', header=None, dtype=float)
-plt.plot(data[0], abs(data[1]))
+plt.plot(data[0], data[1])
 plt.show()
