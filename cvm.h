f414e6e6121bf18c69fa994028a33cb131bb1b37
ssack <ssack@student.ethz.ch>
2019-07-13 14:49:37 +0100

    Renamed bicstab->bicgstab and pass sites object in identity

--- cvm.h
+++ cvm.h
@@ -1,18 +1,20 @@
-MPO I() {
+static auto Iden=[](auto sites) {
     auto ampo = AutoMPO(sites);
-    ampo += "Id", 0;
+    ampo += "Id", 1;
     return MPO(ampo);
-}
+};
 
 MPS conjMPS(MPS psi){
-    auto tmp = psi;
-    for(int i = 1; i <= N; ++i){
-        tmp.Aref(i).conj();
+    auto tmp = psi*1.;
+    for(int i = 0; i < tmp.N(); ++i){
+        tmp.Aref(i+1).conj();
     }
     return tmp;
 }
 
-MPS bicstab(MPO A, MPS b, double tol, auto args){
+
+// CVM solver
+MPS bicgstab(MPO A, MPS b, double tol, int max_it, Args const& args){
 
     MPS x = b;
     MPS r_old = sum(b, -1 * applyMPO(A, x, args));
@@ -26,10 +28,9 @@ MPS bicstab(MPO A, MPS b, double tol, auto args){
     std::complex<double> beta;
     std::complex<double> w;
     double res;
-    int n = 100;
     int k = 0;
 
-    while(k < n){
+    while(k < max_it){
 
         Ap = applyMPO(A, p, args);
         alpha = overlapC(conjMPS(r_old), r_) / overlapC(conjMPS(Ap), r_);
@@ -38,9 +39,9 @@ MPS bicstab(MPO A, MPS b, double tol, auto args){
         w = overlapC(conjMPS(As), s) / overlapC(conjMPS(As), As);
         x = sum(x, sum(alpha * p, w * s, args), args);
         r_new = sum(s, -w * As, args);
-        res = overlapC(conjMPS(r_new), r_new).real();
+        res = sqrt(abs(overlapC(conjMPS(r_new), r_new).real()));
 
-        if(abs(res) <= tol * tol){
+        if(res <= tol){
             std::cout << "Residue = " << res << std::endl;
             break;
         }
@@ -48,22 +49,23 @@ MPS bicstab(MPO A, MPS b, double tol, auto args){
         beta = (alpha / w) * overlapC(conjMPS(r_new), r_) / overlapC(conjMPS(r_old), r_);
         p = sum(r_new, beta * sum(p, -w * Ap, args), args);
         r_old = r_new;
-        k++
+        k++;
 
     };
 
     return x;
 }
 
-double spectral_function(MPS psi, MPO H, MPO S1, MPO S2, double omega, double eta, double energy, double tol, int i, int j) {
+// main CVM function
+static auto spectral_function=[](MPS psi, MPO H, MPO S1, MPO S2, double omega,
+                                 double eta, double energy, double tol, int max_it,
+                                 int maxm, double cut, auto sites) {
 
-    int maxm = get_int_value("Maxm");
-    double cut = get_float_value("Cutoff");
     auto args = Args({"Maxm", maxm, "Cutoff", cut});
     const std::complex<double> z(omega + energy, eta);
-    auto A = sum(z * I(), -1. * H, args);
-    auto b =  applyMPO(S2j, psi, args);
-    auto x = bicstab(A, b, tol, args);
+    auto A = sum(z * Iden(sites), -1. * H, args);
+    auto b =  applyMPO(S2, psi, args);
+    auto x = bicgstab(A, b, tol, max_it, args);
     std::complex<double> G = overlapC(psi, S1, x);
 
     return -G.imag() / M_PI;
