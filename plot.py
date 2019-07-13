94da041395e53bb6759e5971962d49771cec7bc9
ssack <ssack@student.ethz.ch>
2019-07-13 14:51:23 +0100

    Updated plotting

--- plot.py
+++ plot.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file = 'data/cvm_data.csv'
+file = 'data/heisenberg_data.csv'
 data = pd.read_csv(file, delimiter=' ', header=None, dtype=float)
-plt.plot(data[0], data[1])
+
+#todo: compare to ED
+
+plt.plot(data[0], data[1], label='cvm')
+plt.xlabel('omega')
+plt.ylabel('spectral function')
+plt.legend()
 plt.show()
